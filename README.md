09ac0ebe511b1ad98f4757e687c32092468c5a82
Zach Cowell <zach.cowell@gmail.com>
2021-04-26 02:49:39 -0400

    animations handling

--- README.md
+++ README.md
@@ -4,8 +4,21 @@ Takes a 2D spritesheet and converts it to a horizontal spritesheet. Useful for C
 
 ### Example Usage
 
-`node index.js -x 8 -y 14 path/to/current.png -o new_image.png`
+`node index.js -x 8 -y 14 path/to/current.png -o new_image.png -a animations.json`
 
 * x = number of frame columns
 * y = number of frame rows
 * o = new image output (optional, defaults to out.png)
+* a = animations json input (optional)
+
+### Sample animations input
+
+```
+{
+    "idle": {
+        "start": 0,
+        "end": 5,
+        "speed": 2
+    }
+}
+```
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const sizeOf = require('image-size');
 const Jimp = require('jimp');
+const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 
-const linearify = async (framesX, framesY, img, output='out.png') => {
+const linearify = async ({ 
+    framesX, 
+    framesY, 
+    img, 
+    output='out.png'
+}) => {
     if (!framesX) throw new Error('x is a required parameter');
     if (!framesY) throw new Error('y is a required parameter');
     if (!img) throw new Error('img is a required parameter');
@@ -11,7 +17,7 @@ const linearify = async (framesX, framesY, img, output='out.png') => {
     const frameHeight = dimensions.height / framesY;
     const frameWidth = dimensions.width / framesX;
     const currentImage = await Jimp.read(img);
-    const newImg = await new Jimp(newImageWidth, frameHeight);
+    const newImg = new Jimp(newImageWidth, frameHeight);
     for (let x=0; x < framesX; x++){
         for (let y=0; y< framesY; y++){
             newImg.blit(currentImage, 
@@ -26,8 +32,70 @@ const linearify = async (framesX, framesY, img, output='out.png') => {
     newImg.write(output);
 }
 
+const spritesheetify = ({
+    img,
+    framesX, 
+    framesY,
+    animations,
+    spritesheet='spritesheet.css'
+}) => {
+    if (!framesX) throw new Error('x is a required parameter');
+    if (!framesY) throw new Error('y is a required parameter');
+    if (!img) throw new Error('img is a required parameter');
+    const dimensions = sizeOf(img);
+    const newImageWidth = dimensions.width * framesY;
+    const frameHeight = dimensions.height / framesY;
+    const frameWidth = dimensions.width / framesX;
+
+    const allAnimation = `.all {
+        width: ${frameWidth}px;
+        height: ${frameHeight}px;
+        background-image: url("${img}");
+        image-rendering: pixelated;
+        animation: all-animation 2s steps(${framesX * framesY}) infinite;
+    }
+
+    @keyframes all-animation {
+        from { background-position:    0px; }
+          to { background-position: -${newImageWidth}px; }
+     }
+    `
+
+    let animationsToGenerate = [allAnimation];
+    if (animations && fs.existsSync(animations)){
+        const animationStr = fs.readFileSync(animations);
+        const animationObj = JSON.parse(animationStr);
+        for (const animation in animationObj){
+            const {end, start, speed} = animationObj[animation];
+            animationsToGenerate.push(`.${animation} {
+                width: ${frameWidth}px;
+                height: ${frameHeight}px;
+                background-image: url("${img}");
+                image-rendering: pixelated;
+                animation: ${animation}-animation ${speed}s steps(${end-start + 1}) infinite;
+            }
+        
+            @keyframes ${animation}-animation {
+                from { background-position:    ${-1 * start * frameWidth}px; }
+                  to { background-position: ${-1 * (end+1) * frameWidth}px; }
+             }
+            `);
+        }
+    }
+    const cssString = animationsToGenerate.join('\n');
+    fs.writeFileSync(spritesheet, cssString, 'utf8');
+}
+
 try {
-    linearify(argv['x'], argv['y'], argv['_'][0], argv['o']);
+    const framesX = argv['x'];
+    const framesY = argv['y'];
+    const img = argv['_'][0];
+    const output = argv['o'];
+    const animations = argv['a'];
+    
+    linearify({ framesX, framesY, img, output });
+    spritesheetify({ framesX, framesY, img, animations });
+
 } catch (e){
     console.error('Error: ' + e.message);
 }
